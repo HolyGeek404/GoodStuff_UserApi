8663ea29d588b05078d4fa8b64569da6e4df5f06
HolyGeek404 <wiktorzme@gmail.com>
2025-05-27 11:25:03 +0200

    Working on CosmosDB client

--- WebApi/ServiceCollectionExtensions.cs
+++ WebApi/ServiceCollectionExtensions.cs
@@ -1,4 +1,11 @@
-﻿using Model.DataAccess;
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Azure.Cosmos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+using Model.DataAccess;
+using Model.DataAccess.Context;
 using Model.DataAccess.Interfaces;
 using Model.Services;
 using Model.Services.Interfaces;
@@ -12,6 +19,55 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IUserDao, UserDao>();
         services.AddScoped<IUserService, UserService>();
         services.AddScoped<IPasswordService, PasswordService>();
+        return services;
+    }
+
+    public static IServiceCollection AddAzureConfig(this IServiceCollection services, IConfigurationManager configuration)
+    {
+        var azureAd = configuration.GetSection("AzureAd");
+
+        configuration.AddAzureKeyVault(new Uri(azureAd["KvUrl"]), new DefaultAzureCredential());
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApi(azureAd);
+
+        return services;
+    }
+
+    public static IServiceCollection AddDataBaseConfig(this IServiceCollection services, IConfigurationManager configuration)
+    {
+        services.AddDbContext<PgpContext>(options => options.UseSqlServer(configuration.GetConnectionString("SqlDb")));
+        services.AddSingleton(s => new CosmosClient(configuration.GetConnectionString("CosmosDB")));
+        return services;
+    }
+
+    public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new List<string>()
+                }
+            });
+        });
         return services;
     }
 }
